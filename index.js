a2ed949845b7c58b49a59d43b448448a559374f7
max.rudenko <max.rudenk@outlook.com>
2019-07-05 10:39:57 +0300

    add mathod

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const instance_locator_id = "082d1264-5771-48aa-9828-a1f0e3231330";
 const chatkit_secret = "9f524a7a-d414-45f8-b2a9-498267df3cd6:Tywp2QvyqFS6tGkq67whRFkmiCZgCZdnnNp6SsGR1W8=";
 
 const chatkit = new Chatkit.default({
-  instanceLocator: `v1:us1:${instance_locator_id}`,
-  key: chatkit_secret
+  instanceLocator: 'v1:us1:082d1264-5771-48aa-9828-a1f0e3231330',
+  // instanceLocator: `v1:us1:${instance_locator_id}`,
+  // key: chatkit_secret
+  key: "9f524a7a-d414-45f8-b2a9-498267df3cd6:Tywp2QvyqFS6tGkq67whRFkmiCZgCZdnnNp6SsGR1W8="
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,33 +23,71 @@ app.get("/", (req, res) => {
   res.send("all green!");
 });
 
+app.post('/auth', (req, res) => {
+  const { userId } = req.body;
+  const authData = chatkit.authenticate({
+    userId
+  });
+
+  res.status(authData.status)
+     .set(authData.headers)
+     .send(authData.body);
+})
+
+
+app.get("/users", (req, res) => {
+  
+  chatkit.getUsers({
+    limit: 200
+  })
+    .then((user) => {
+      console.log('Success', user);
+    }).catch((err) => {
+      console.log(err);
+    });
+  
+})
+
+
 app.post("/users", (req, res) => {
   const { username } = req.body;
- 
-  chatkit
-    .createUser({
-      id: username,
-      name: username
-    })
-    .then(() => {
-      console.log("AAA: 2 ok")
-      res.sendStatus(201);
-    })
-    .catch(error => {
-      console.log("AAA: 2 error" )
-      console.log(error )
-      if (error.error === "services/chatkit/user_already_exists") {
-        res.sendStatus(200);
-      } else {
-        let statusCode = error.status;
-        if (statusCode >= 100 && statusCode < 600) {
-          res.status(statusCode);
-        } else {
-          res.status(500);
-        }
-      }
+  chatkit.createUser({
+    username
+  })
+    .then((user) => {
+      console.log('Success', user);
+    }).catch((err) => {
+      console.log(err);
     });
-});
+  
+})
+
+// app.post("/users", (req, res) => {
+//   const { username } = req.body;
+ 
+//   chatkit
+//     .createUser({
+//       username
+//     })
+//     .then(() => {
+//       console.log("AAA: 2 ok")
+//       res.sendStatus(201);
+//     })
+//     .catch(error => {
+//       console.log("AAA: 2 error" )
+//       console.log(error )
+//       if (error.error === "services/chatkit/user_already_exists") {
+//         res.sendStatus(200);
+//       } else {
+//         let statusCode = error.status;
+//         if (statusCode >= 100 && statusCode < 600) {
+//           res.status(statusCode);
+//         } else {
+//           res.status(500);
+//         }
+//       }
+//     });
+// });
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, err => {
